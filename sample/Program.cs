c3f007c664e1a5cb586e1c71d045b462a2fee9e6
Christopher Broome <ChrisBroome7@gmail.com>
2013-10-30 23:32:30 -0400

    Add ServiceStack NuGet packages

--- sample/Program.cs
+++ sample/Program.cs
@@ -1,14 +1,51 @@
 using System;
+using ServiceStack.ServiceInterface;
+using ServiceStack.WebHost.Endpoints;
 
 namespace sample
 {
+  public class Hello
+  {
+    public string Name { get; set; }
+  }
+
+  public class HelloResponse
+  {
+    public string Result { get; set; }
+  }
+
+  public class HelloService : Service
+  {
+    public object Any( Hello request )
+    {
+      return new HelloResponse{ Result = "Hello!" + request.Name };
+    }
+  }
+
+  public class AppHost : AppHostHttpListenerBase
+  {
+    public AppHost() : base( "HttpListener Self-Host", typeof( HelloService ).Assembly )
+    {
+    }
+
+    public override void Configure( Funq.Container container )
+    {
+      Routes.Add<Hello>( "/hello" );
+      Routes.Add<Hello>( "/hello/{Name}" );
+    }
+  }
+
   public class Program
   {
-    public static int Main(string[] args)
+    public static int Main( string[] args )
     {
-      Console.WriteLine( @"Hello World!" );
+      var listeningOn = args.Length == 0 ? "http://localhost:1337" : args[0];
+      var appHost = new AppHost();
+      appHost.Init();
+      appHost.Start( listeningOn );
+      Console.WriteLine( "Apphost created at {0}, listening on {1}", DateTime.UtcNow, listeningOn );
+      Console.ReadKey();
       return 0;
     }
   }
 }
-
