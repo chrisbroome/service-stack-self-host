fa774c3c207cf66776d853501052efc980abdfce
Christopher Broome <ChrisBroome7@gmail.com>
2013-10-30 23:32:45 -0400

    Use verbatim strings

--- sample/Program.cs
+++ sample/Program.cs
@@ -18,20 +18,20 @@ namespace sample
   {
     public object Any( Hello request )
     {
-      return new HelloResponse{ Result = "Hello!" + request.Name };
+      return new HelloResponse{ Result = @"Hello!" + request.Name };
     }
   }
 
   public class AppHost : AppHostHttpListenerBase
   {
-    public AppHost() : base( "HttpListener Self-Host", typeof( HelloService ).Assembly )
+    public AppHost() : base( @"HttpListener Self-Host", typeof( HelloService ).Assembly )
     {
     }
 
     public override void Configure( Funq.Container container )
     {
-      Routes.Add<Hello>( "/hello" );
-      Routes.Add<Hello>( "/hello/{Name}" );
+      Routes.Add<Hello>( @"/hello" );
+      Routes.Add<Hello>( @"/hello/{Name}" );
     }
   }
 
@@ -39,11 +39,11 @@ namespace sample
   {
     public static int Main( string[] args )
     {
-      var listeningOn = args.Length == 0 ? "http://localhost:1337" : args[0];
+      var listeningOn = args.Length == 0 ? @"http://localhost:1337/" : args[0];
       var appHost = new AppHost();
       appHost.Init();
       appHost.Start( listeningOn );
-      Console.WriteLine( "Apphost created at {0}, listening on {1}", DateTime.UtcNow, listeningOn );
+      Console.WriteLine( @"Apphost created at {0}, listening on {1}", DateTime.UtcNow, listeningOn );
       Console.ReadKey();
       return 0;
     }
